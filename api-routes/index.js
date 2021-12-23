818e736de30412bb0b78222d052a1164ca4fc756
mauriciobranda <mauricio.20@gmail.com>
2021-12-23 18:50:40 -0300

    Comitando

--- api-routes/index.js
+++ api-routes/index.js
@@ -7,5 +7,5 @@ const obras = ["Saab", "Volvo", "BMW"];
 module.exports = app => {
     app.use(bodyParser.json());
     app.get('/obras', (req, res) => res.send(obras));
-
+    app.post('/obras', (req, res) => res.send(obras.push(req.body.titulo)));
 }
