f96f906e977393600551771646fce00105110fc8
mauriciobranda <mauricio.20@gmail.com>
2021-12-24 12:00:51 -0300

    Update

--- api-routes/index.js
+++ api-routes/index.js
@@ -4,14 +4,27 @@ const obras = [];
 
 module.exports = app => {
     app.use(bodyParser.json());
-    app.get('/obras', (req, res) => res.send(obras));
+    app.get('/obras', (request, response) => response.send(obras));
 
     app.post('/obras', function(request, response){
-        
+        const id = request.params.id;
+
+        //const idExiste = obras.some(obra => obra.id === id);
+        //console.log(idExiste); // true
+
         const obra = request.body;
         obras.push(obra);
         response.send('Post titulo')
         
     });
+
+    app.put('/obras/:id', function(request, response){
+        const id = request.params.id;
+        const obra = request.body;
+        obras[id] = obra;
+        console.log(obra);
+        response.send('Put titulo')
+    })
+    
 
 }
