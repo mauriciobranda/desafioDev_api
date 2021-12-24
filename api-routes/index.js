13993a07b1f06e2960960e15b8a3b84985dadb5f
mauriciobranda <mauricio.20@gmail.com>
2021-12-23 21:19:15 -0300

    [Update] Added POST method

--- api-routes/index.js
+++ api-routes/index.js
@@ -1,11 +1,17 @@
 const bodyParser = require('body-parser');
 
-
 const obras = ["Saab", "Volvo", "BMW"];
-
 
 module.exports = app => {
     app.use(bodyParser.json());
     app.get('/obras', (req, res) => res.send(obras));
-    app.post('/obras', (req, res) => res.send(obras.push(req.body.titulo)));
+
+    app.post('/obras', function(request, response){
+        
+        const obra = request.body;
+        obras.push(obra);
+        response.send('Post titulo')
+        
+    });
+
 }
